ca2019f7587a6c3dfb99def0f9a5d4c9909ff1ce
ATS88GR <turok.artyom@gmail.com>
2023-07-11 07:43:08 +0300

    add swagger annotations

--- src/main/java/com/education/projects/cars/manager/carsmanager/controller/AutoController.java
+++ src/main/java/com/education/projects/cars/manager/carsmanager/controller/AutoController.java
@@ -2,7 +2,9 @@ package com.education.projects.cars.manager.carsmanager.controller;
 
 import com.education.projects.cars.manager.carsmanager.model.Car;
 import com.education.projects.cars.manager.carsmanager.service.DBCarServiceImpl;
-//import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,42 +13,54 @@ import java.util.Collection;
 
 @RestController
 @Slf4j
-//@Tag(name = "Cars API")
+@Tag(name = "Cars API")
 public class AutoController {
 
     @Autowired
     private DBCarServiceImpl dbCarServiceImpl;
 
+    @Operation(summary = "Creates new row in database with car information",
+            description = "Returns created car information from database")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved")
     @PostMapping("/cars")  //url
-    /**
-     * Creates new row in database with car information
-     */
     public Car createCar(@RequestBody Car car){
         log.info("Create car ={}", car);
-        return dbCarServiceImpl.createAuto(car);  //returns object from database
+        return dbCarServiceImpl.createAuto(car);
     }
+
+    @Operation(summary = "Updates car information by id",
+            description = "Returns updated car information from database")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved")
     @PutMapping("/cars/{id}")
     public Car updateCar(@RequestBody Car car, @PathVariable Long id){
         log.info("Update car with id ={}, update car info {}", id, car);
         return dbCarServiceImpl.updateAuto(car, id);
     }
 
+    @Operation(summary = "Gets information about all cars from database",
+            description = "Returns collection of car objects from database")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved")
     @GetMapping("/cars")
     public Collection <Car> getCars() {
+        log.info("Get all car info");
         return dbCarServiceImpl.getAllCars();
     }
 
+    @Operation(summary = "Gets car by id",
+            description = "Returns id car information from database")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved")
     @GetMapping("/cars/{id}")
     public Car getCarById(@PathVariable Long id) {
-
+        log.info("Gets car with id ={}", id);
         return dbCarServiceImpl.getCarById(id);
     }
 
+    @Operation(summary = "Deletes car by id",
+            description = "Removes the row with id from database")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved")
     @DeleteMapping("/cars/{id}")
-    /**
-     * removes the row with id from database
-     */
     public void deleteCarById(@PathVariable Long id) {
+        log.info("Deletes car with id ={}", id);
         dbCarServiceImpl.deleteCarById(id);
     }
 
--- src/main/java/com/education/projects/cars/manager/carsmanager/model/Car.java
+++ src/main/java/com/education/projects/cars/manager/carsmanager/model/Car.java
@@ -1,5 +1,6 @@
 package com.education.projects.cars.manager.carsmanager.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,11 +9,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class Car {
-
+    @Schema (name = "id", description = "Car id", example = "1")
     private int id;
+    @Schema (name = "year", description = "Year of production", example = "2023")
     private int year;
+    @Schema (name = "brand", description = "Car manufacturer brand", example = "VW")
     private String brand;
+    @Schema (name = "model", description = "Car model", example = "Polo")
     private String model;
+    @Schema (name = "cost", description = "Car cost", example = "5000")
     private int cost;
 
     public Car() {
